79210c41e35b60ba2b33fc90d0d8268abbf506ef
Mostafa Kamal <mostafajunayed20@gmail.com>
2023-04-26 05:36:44 -0400

    keyboard

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(props) {
+  return (
+    <div>
+      Tittle, Log Out, Settings, show leaderboard, if its randomly gen user
+      soemthing else
+    </div>
+  );
+}
