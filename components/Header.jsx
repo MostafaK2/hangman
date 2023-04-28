7681ee9d683cdf2c5ed6c452b85fd93ab98bb7bc
Abishek Goutham <abgo4601@colorado.edu>
2023-04-27 23:46:30 -0600

    Merges hangman-logic #2

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,63 +1,37 @@
 import React from "react";
 import styles from "../styles/Header.module.css";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 
-export default function Header({ page, onClick }) {
-  const router = useRouter();
+export default function Header({page, onClick}) {
+    const router = useRouter();
 
-<<<<<<< Updated upstream
-  const routeLeaderboard = async (e) => {};
-
-  return (
-    <div className={styles["parent"]}>
-      <h1>Hangman</h1>
-      <div className={styles["header-buttons"]}>
-        {page !== "leaderboard" ? (
-          <button
-            className={styles["button-28"]}
-            onClick={() => router.push("/leaderboard")}
-          >
-            Leaderboard
-          </button>
-        ) : (
-          <button
-            className={styles["button-28"]}
-            onClick={() => router.push("/")}
-          >
-            Game
-          </button>
-        )}
-        <button className={styles["button-28"]}>Create Custom Game</button>
-
-        <button className={styles["button-28"]} onClick={onClick}>
-          Reset Game
-        </button>
-      </div>
-      <button className={styles["button-28"]}> Settings</button>
-    </div>
-  );
-=======
     return (
         <div className={styles["parent"]}>
             <h1>Hangman</h1>
             <div className={styles["header-buttons"]}>
-                {page !== 'leaderboard' ?
-                    <button className={styles["header-button"]} onClick={() =>
-                        router.push('/leaderboard')}>Leaderboard</button> :
-                    <button className={styles["header-button"]} onClick={() =>
-                        router.push('/')}>Game</button>}
-                <button className={styles["header-button"]}>Create Custom Game</button>
-                <button className={styles["header-button"]}>
-                    {" "}
-                    Settings with Logout
-                </button>
-                <button className={styles["header-button"]} onClick={onClick}>
+                {page !== "leaderboard" ? (
+                    <button
+                        className={styles["button-28"]}
+                        onClick={() => router.push("/leaderboard")}
+                    >
+                        Leaderboard
+                    </button>
+                ) : (
+                    <button
+                        className={styles["button-28"]}
+                        onClick={() => router.push("/")}
+                    >
+                        Game
+                    </button>
+                )}
+                <button className={styles["button-28"]}>Create Custom Game</button>
+
+                <button className={styles["button-28"]} onClick={onClick}>
                     Reset Game
                 </button>
             </div>
-            <h2>Username</h2>
+            <button className={styles["button-28"]}> Settings</button>
         </div>
     );
->>>>>>> Stashed changes
 }
