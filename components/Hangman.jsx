e4664dd866f7aeb32afdac37373bc6fbf4038ff6
Mostafa Kamal <mostafajunayed20@gmail.com>
2023-04-27 20:42:52 -0400

    changed up UI for texts view

--- components/Hangman.jsx
+++ components/Hangman.jsx
@@ -9,7 +9,7 @@ const LEFT_ARM = <div className={styles.leftarm} />;
 const RIGHT_ARM = <div className={styles.rightarm} />;
 const LEFT_LEG = <div className={styles.leftleg} />;
 const RIGHT_LEG = <div className={styles.rightleg} />;
-const HANGMAN = [HEAD, BODY, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG]
+const HANGMAN = [HEAD, BODY, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG];
 
 // drawing
 export default function Hangman({ word, correct, incorrect }) {
@@ -18,7 +18,6 @@ export default function Hangman({ word, correct, incorrect }) {
   return (
     <div className={styles.parent}>
       <div style={{ position: "relative", alignItems: "center" }}>
-        
         {HANGMAN.slice(0, incorrect.length)}
         <div className={styles.noose2} />
         <div className={styles.noose1} />
@@ -28,13 +27,23 @@ export default function Hangman({ word, correct, incorrect }) {
       <div className={styles.child}>
         {word.map((elem) => {
           var temp = "";
-          if (correct.includes(elem)) {
+          const bool = correct.includes(elem) || incorrect.length > 5;
+          if (bool) {
             temp = elem;
           }
           return (
             <div>
-              <div>{temp}</div>
-              <div>____</div>
+              <div
+                className={`${styles["box"]} ${
+                  correct.includes(elem)
+                    ? styles["correct"]
+                    : incorrect.length < 6
+                    ? ""
+                    : styles["incorrect"]
+                }`}
+              >
+                <p>{temp}</p>
+              </div>
             </div>
           );
         })}
